769266979344409d78a58a771312af5bab62f46f
Rafael Bicalho <rbmbika@gmail.com>
2016-12-28 15:25:18 -0200

    Implement taxes calculation for non logged users

--- static/js/unlogged.js
+++ static/js/unlogged.js
@@ -1,15 +1,48 @@
 $(function() {
+  var subTotal = 0;
+  var total = 0;
+
+  // Updates the invoice header when client's name is changed
   $('.company-info h2').on('keyup', '.name', function() {
     $('.invoice__branding h2').text($(this).val());
   });
 
+  // Adds a new tax entry and updates the total price
+  $('.tax-info .add-button').click(function() {
+    var $row = $(this).closest('tr');
+
+    if ($row.find('.js-tax-value').val()) {
+      var $table = $row.parent();
+      var $tax = $row.clone();
+
+      $row.find('input').val('');
+
+      $tax.find('.add-button').remove();
+      $table.prepend($tax);
+
+      if (subTotal) {
+        updateFinalPrice();
+      }
+    }
+  });
+
+  // Updates total price when a tax values is changed
+  $('.tax-info').on('change', '.js-tax-value', function() {
+    // If the row has the "add-button" let the button click handle the update
+    if (!$(this).closest('tr').find('.add-button').length) {
+      if (subTotal) {
+        updateFinalPrice();
+      }
+    }
+  });
+
+  // Updates the invoice content with the data from the uploaded CSV file
   $('body').on('submit', '#upload-timesheet', function() {
     var $input = $(this).children('input[type=file]');
 
     if ($input[0].files.length) {
       var onComplete = function(results) {
         var chunk = 25;  // Number of rows that fit one page
-        var total = 0;
         var slice;
         var $page;
         var $br;
@@ -28,7 +61,7 @@ $(function() {
               $tr += '<td class="duration">' + row[11];
               $tr += '<td class="amount">' + row[13];
 
-              total += parseFloat(row[13]);
+              subTotal += parseFloat(row[13]);
 
               $page.find('.timesheet tbody').append($tr);
             }
@@ -38,8 +71,8 @@ $(function() {
           $('#js-dinamic-content').append($page.removeClass('hidden'));
         }
 
-        if (total) {
-          $('.invoice__amount').text('$ ' + total.toFixed(2));
+        if (subTotal) {
+          updateFinalPrice();
         }
 
         $input.val('');
@@ -50,4 +83,22 @@ $(function() {
 
     return false;
   });
+
+  // Auxiliar functions
+  // ------------------
+
+  var updateFinalPrice = function() {
+    var $inputs = $('.js-tax-value');
+    var taxes = 0;
+
+    $inputs.each(function(i, input) {
+      if (input.value) {
+        taxes += parseInt(input.value);
+      }
+    });
+
+    total = subTotal * (1 + taxes / 100);
+
+    $('.invoice__amount').text('$ ' + total.toFixed(2));
+  }
 });
